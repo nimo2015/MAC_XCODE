a15ce43c2fc25b79108b777997ffab556a700005
SUNIANFU <SUNIANFU@sunianfudemacbook-air.local>
2015-12-02 19:51:11 +0800

    TEST

---
+++ MACOS_TEST/MACOS_TEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MACOS_TEST
+//
+//  Created by SUNIANFU on 15/12/2.
+//  Copyright © 2015年 SUNIANFU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MACOS_TEST/MACOS_TEST/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MACOS_TEST
+//
+//  Created by SUNIANFU on 15/12/2.
+//  Copyright © 2015年 SUNIANFU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
